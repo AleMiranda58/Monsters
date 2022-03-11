d752b90d39dc9ce7e8508b2c2bc74d223f1d748e
Alejandra Miranda Reyes <ale.miranda890@gmail.com>
2022-03-11 16:46:03 -0500

    convertir search-box class component into a functional component

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -1,19 +1,15 @@
-import { Component } from "react";
 import './search-box.css'
 
-class SearchBox extends Component {
-    render(){
-        return(
-            <div>
-                <input 
-                    className={this.props.className} 
-                    type='search'
-                    placeholder={this.props.placeholder} 
-                    onChange={this.props.onChangeHandler}
-                />
-            </div>
-        )
-    }
-}
+const SearchBox = ({ className, placeholder, onChangeHandler}) => (
+        <div>
+            <input 
+                className={className} 
+                type='search'
+                placeholder={placeholder} 
+                onChange={onChangeHandler}
+            />
+        </div>
+)
+
 
 export default SearchBox
