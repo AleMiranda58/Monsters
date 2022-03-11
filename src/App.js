c27978b3f91c7d7318a416c0c7383a0e855c656e
Alejandra Miranda Reyes <ale.miranda890@gmail.com>
2022-03-11 12:21:31 -0500

    Filtering data according to user inputs

--- src/App.js
+++ src/App.js
@@ -7,20 +7,45 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     }
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({monsters: users}))
+      .then(users => this.setState({monsters: users})
+    )
   }
 
+  onSearchChange = (event) => {
+    const targetValue = event.target.value.toLowerCase()
+    this.setState(
+      () => {
+      return {searchField: targetValue }
+    })
+  }
+
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField)
+    });
+
     return (  
       <div className='App'>
+      <input 
+        className='search-input' 
+        type='search'
+        placeholder='Search Monster' 
+        onChange={onSearchChange}
+       />
+
       {
-        this.state.monsters.map(monsters => (
+        filteredMonsters.map(monsters => (
         <h1 key={monsters.id}> { monsters.name }  </h1>
       ))}
       </div>
